932ce50cb88f6e24390ae8ec0a11aee3bc504197
Rishabh Bajpai <f20170121@goa.bits-pilani.ac.in>
2018-10-19 15:59:41 +0530

    check 1

--- app/src/main/java/com/a2599gmail/hari/sosmap/AddRequestFragment.java
+++ app/src/main/java/com/a2599gmail/hari/sosmap/AddRequestFragment.java
@@ -1,20 +1,38 @@
 package com.a2599gmail.hari.sosmap;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class AddRequestFragment extends Fragment {
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Button locationButton;
+    private Button submitRequest;
+    private TextView locationView;
 
 
     public AddRequestFragment() {
         // Required empty public constructor
     }
+
     public static AddRequestFragment newInstance() {
         AddRequestFragment fragment = new AddRequestFragment();
         Bundle args = new Bundle();
@@ -22,12 +40,68 @@ public class AddRequestFragment extends Fragment {
         return fragment;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_request, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_request, container, false);
+
+        locationButton = view.findViewById(R.id.buttonLocation);
+        submitRequest = view.findViewById(R.id.SubmitRequestButton);
+        locationView = view.findViewById(R.id.textViewLocation);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                locationView.append("\n"+location.getLatitude() + "," + location.getLongitude());
+                Log.d("Location", "onLocationChanged: ");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        checkLocation();
+        return view;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 1:
+                checkLocation();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void checkLocation(){
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(Build.VERSION.SDK_INT > 23)
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET},1);
+            return;
+        }
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        });
+    }
 }
--- app/src/main/java/com/a2599gmail/hari/sosmap/Request.java
+++ app/src/main/java/com/a2599gmail/hari/sosmap/Request.java
@@ -1,4 +1,72 @@
 package com.a2599gmail.hari.sosmap;
 
+import android.location.Location;
+
+import java.util.ArrayList;
+
 public class Request {
+    private String name;
+    private boolean isFullfilled;
+    private Location location;
+    private ArrayList<String> imgURL;
+    private ArrayList<String> requestType;
+    private String desc;
+
+    public Request(String name, boolean isFullfilled, ArrayList<String> requestType, String desc) {
+        this.name = name;
+        this.isFullfilled = isFullfilled;
+        this.requestType = requestType;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFullfilled() {
+        return isFullfilled;
+    }
+
+    public void setFullfilled(boolean fullfilled) {
+        isFullfilled = fullfilled;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public ArrayList<String> getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(ArrayList<String> imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public ArrayList<String> getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(ArrayList<String> requestType) {
+        this.requestType = requestType;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+
 }
